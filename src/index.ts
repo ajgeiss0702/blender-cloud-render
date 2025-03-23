75f5350877b36dd2ff7e09748063ba354d87a94e
ajgeiss0702 <aj@ajg0702.us>
2025-03-22 21:22:07 -0700

    prevent wget progress from spamming logs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import {exec, spawn} from 'child_process';
 console.log("hello world!");
 exec("wget https://pub-dd273e04901f409f8dbd9aee5b39ded6.r2.dev/dounut_small.blend", (error, stdout, stderr) => {
     if(error) console.log(error);
-    console.log(stdout.toString());
+    // lines are filter to exclude all of the progress lines from spamming the logs
+    console.log(stdout.split("\n").filter(l => !l.includes("..........")).join("\n").toString());
     console.log(stderr.toString());
     if(!error) {
         const render = spawn("/usr/local/blender/blender", "dounut_small.blend -b -f 160 -- --cycles-device OPTIX".split(" "));
